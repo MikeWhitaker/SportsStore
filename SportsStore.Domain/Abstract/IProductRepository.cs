074e622790a9241dc7e108216df475f9a0c51e4e
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2017-05-20 13:18:42 +0200

    Before removing the manager controller. chapter 11 p. 298

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract {
+	public interface IProductRepository {
+
+		IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
+
+    }
+}
---
+++ SportsStore/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using SportsStore.WebUI.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Binders;
+
+namespace SportsStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+			// in het boek wordt er ook WebApiConfig.Register(GlobalConfiguration.Configuration);
+			ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+			ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+
+		}
+    }
+}
